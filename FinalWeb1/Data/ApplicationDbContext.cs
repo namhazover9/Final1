1b371f8198e922f7e3ba18d2751fa4a82ec291b6
namhazover9 <88364629+namhazover9@users.noreply.github.com>
2024-02-09 16:09:36 +0700

    Add Category

---
+++ FinalWeb1/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using FinalWeb1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalWeb1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Sweater", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Hoodie", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Shoe", DisplayOrder = 3 }
+                );
+        }
+    }
+    
+    
+}
